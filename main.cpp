38c5d8006572608cab53a37873e3c9d2f7253510
Adrian <adrian@mxlinux.org>
2020-12-21 16:13:47 -0500

    move license.html to /help

--- main.cpp
+++ main.cpp
@@ -24,34 +24,40 @@
  * along with mx-welcome.  If not, see <http://www.gnu.org/licenses/>.
  **********************************************************************/
 
+#include <QApplication>
+#include <QIcon>
+#include <QLibraryInfo>
+#include <QLocale>
+#include <QTranslator>
+
 #include "mainwindow.h"
 #include <unistd.h>
-#include <QApplication>
-#include <QTranslator>
-#include <QLocale>
-#include <QIcon>
 
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    a.setWindowIcon(QIcon::fromTheme("mx-welcome"));
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon::fromTheme(app.applicationName()));
 
     QTranslator qtTran;
-    qtTran.load(QString("qt_") + QLocale::system().name());
-    a.installTranslator(&qtTran);
+    if (qtTran.load(QLocale::system(), "qt", "_", QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+        app.installTranslator(&qtTran);
+
+    QTranslator qtBaseTran;
+    if (qtBaseTran.load("qtbase_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+        app.installTranslator(&qtBaseTran);
 
     QTranslator appTran;
-    appTran.load(QString("mx-welcome_") + QLocale::system().name(), "/usr/share/mx-welcome/locale");
-    a.installTranslator(&appTran);
+    if (appTran.load(app.applicationName() + "_" + QLocale::system().name(), "/usr/share/" + app.applicationName() + "/locale"))
+        app.installTranslator(&appTran);
 
     if (getuid() != 0) {
         MainWindow w;
         w.show();
-        return a.exec();
+        return app.exec();
     } else {
         QApplication::beep();
-        QMessageBox::critical(0, QString::null,
+        QMessageBox::critical(nullptr, QString::null,
                               QApplication::tr("You must run this program as normal user."));
         return 1;
     }
